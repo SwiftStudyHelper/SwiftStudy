5563fee68ba254be4b0dfbf41b79378b14848dd8
ZhuKaiQi <zhukaiqi@l2cplat.com>
2016-06-04 10:44:04 +0800

    no message

--- SwiftStudy/ViewController.swift
+++ SwiftStudy/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func  viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated);
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
