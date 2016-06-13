4fbb8c8e8b478506d88789a2f8e58ee542b0ad3f
YangLi <15830034@qq.com>
2016-06-13 09:32:18 +0800

    11111

---
+++ SwiftStudy/RootFiles(框架)/URLFile.swift
@@ -0,0 +1,11 @@
+//
+//  URLFile.swift
+//  SwiftStudy
+//
+//  Created by liyang@l2cplat.com on 16/6/13.
+//  Copyright © 2016年 yang_li828@163.com. All rights reserved.
+//
+
+import Foundation
+
+    let baiduNewsTouTiaoUrl = "http://apis.baidu.com/3023/news/channel"
