8a28f3def02c10e20280f5d032973b1b581dc85e
YangLi <15830034@qq.com>
2016-06-13 18:17:28 +0800

    修改zebu

---
+++ SwiftStudy/周斌/ZBHomePageViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ZBHomePageViewController.swift
+//  SwiftStudy
+//
+//  Created by liyang@l2cplat.com on 16/6/13.
+//  Copyright © 2016年 yang_li828@163.com. All rights reserved.
+//
+
+import UIKit
+
+class ZBHomePageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
