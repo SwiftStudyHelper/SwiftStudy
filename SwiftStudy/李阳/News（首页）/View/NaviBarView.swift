929fd861ee0196c6f83292b4a830ddd79257d74f
YangLi <15830034@qq.com>
2016-06-13 11:38:56 +0800

    添加scrollView

--- SwiftStudy/李阳/News（首页）/View/NaviBarView.swift
+++ SwiftStudy/李阳/News（首页）/View/NaviBarView.swift
@@ -10,22 +10,60 @@ import UIKit
 
 class NaviBarView: UIView {
     
+    let btnWidth:CGFloat = 50
+    
     var titleBarScrollView:UIScrollView = UIScrollView()
+    
+    var redView:UIView = UIView()
+    
+    let titles = ["头条","国际","科技","体育","苹果","社会","美女","段子"]
     
     //重载父类的init方法
     override init(frame: CGRect) {
         
         super.init(frame: frame)
         
-        self.titleBarScrollView.frame = CGRectMake(12, 2, SCREEN_W-24, 40)
-        
-        self.titleBarScrollView.backgroundColor = RGBA(164, G:164 , B: 164, A: 1)
-        
-        self.addSubview(self.titleBarScrollView)
+        createSubViews()
+       
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func createSubViews() {
+        
+        self.backgroundColor = RGBA(251, G: 251, B: 251, A: 1)
+        
+        
+
+        self.titleBarScrollView.frame = CGRectMake(12, 2, SCREEN_W-24, 42)
+        
+        self.titleBarScrollView.backgroundColor = UIColor.clearColor()
+        
+        self.titleBarScrollView.contentSize = CGSizeMake(btnWidth*8, 42)
+        
+        self.addSubview(self.titleBarScrollView)
+        
+        for i in 0 ..< 8 {
+            
+            let btn:UIButton = UIButton(type:.Custom)
+            
+            btn.frame = CGRectMake(CGFloat(Float(i))*btnWidth, 0, btnWidth, 40)
+            
+            btn.setTitle(titles[i], forState: UIControlState.Normal)
+            
+            btn.setTitleColor(RGBA(154, G: 154, B: 154, A: 1), forState: UIControlState.Normal)
+            self.titleBarScrollView.addSubview(btn)
+        }
+        
+        redView.frame = CGRectMake(0, 40, btnWidth, 2)
+        
+        redView.backgroundColor = UIColor.redColor()
+        
+        self.titleBarScrollView.addSubview(redView)
+        
+        
     }
 
 }
