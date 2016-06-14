ee5ecc7b4e33ce5a92d959c58796de54a73811fc
YangLi <15830034@qq.com>
2016-06-14 15:04:50 +0800

    1111

--- SwiftStudy/李阳/News（首页）/Controller/BeautyGirlViewController.swift
+++ SwiftStudy/李阳/News（首页）/Controller/BeautyGirlViewController.swift
@@ -13,7 +13,7 @@ class BeautyGirlViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
 
     
