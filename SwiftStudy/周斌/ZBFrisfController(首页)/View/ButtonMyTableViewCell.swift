03998b2e84ad0fbff98496813068c1bfc6992006
YangLi <15830034@qq.com>
2016-06-15 14:05:48 +0800

    自定义CELL

---
+++ SwiftStudy/周斌/ZBFrisfController(首页)/View/ButtonMyTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  ButtonMyTableViewCell.swift
+//  SwiftStudy
+//
+//  Created by liyang@l2cplat.com on 16/6/15.
+//  Copyright © 2016年 yang_li828@163.com. All rights reserved.
+//
+
+import UIKit
+
+class ButtonMyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+
+    
+}
